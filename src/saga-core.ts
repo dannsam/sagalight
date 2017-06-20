aa0ee0c22a2db5f0c07b51e60cc865e9c385448b
dannsam <daniil.samoylov@pushpay.com>
2017-06-21 10:12:38 +1200

    Exporting all the things from the main module

--- src/saga-core.ts
+++ src/saga-core.ts
@@ -1,1 +1,9 @@
 export { run as runSaga } from './core/run';
+export { Task } from './core/task';
+export { Stream } from './core/stream';
+
+export { cancelled } from './effects/cancelled';
+export { delay } from './effects/delay';
+export { fork } from './effects/fork';
+export { promiseResolver } from './effects/promiseResolver';
+export { take } from './effects/take';
